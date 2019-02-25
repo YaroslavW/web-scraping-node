ab742181eabfe9323a6379e40ffab007d7ef93c6
YaroslavW <doctor355@yahoo.com>
2019-02-25 10:30:33 +0300

    received data from url as an array

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const url = 'https://news.ycombinator.com';
 
 axios.get(url)
   .then(response => {
-    // let html = response.data;
-    console.log(response.data);
+    let html = response.data;
+    console.log(getData(html));
   })
   .catch(error => {
     console.log(error);
   })
+
+  let getData = html => {
+      data = [];
+      const $ = cheerio.load(html);
+      $('table.itemlist tr td:nth-child(3)').each((i, elem) =>{
+          data.push({
+              title: $(elem).text(),
+              link : $(elem).find('a.storylink').attr('href')
+          });
+      });
+    //   console.log(data);
+    return data;
+    }
+
+    // getData(response.data);
